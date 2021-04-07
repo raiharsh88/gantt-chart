47aec59c89b5d08b5f22da331d7e0e916a7ea6ee
raiharsh88 <raiharsh88@gmail.com>
2021-04-07 19:35:14 +0000

    regular backup

---
+++ components/chart.jsx
@@ -0,0 +1,72 @@
+import { Chart } from 'react-google-charts'
+
+
+
+
+
+export default function Gantt(props) {
+
+    return (
+        <div className="chart-div" >
+            <Chart
+                width={'100%'}
+                height={'400px'}
+                chartType="Gantt"
+                loader={<div>Loading Chart</div>}
+                data={[
+                    [
+                        { type: 'string', label: 'Task ID' },
+                        { type: 'string', label: 'Task Name' },
+                        { type: 'string', label: 'Status' },
+                        { type: 'date', label: 'Start Date' },
+                        { type: 'date', label: 'End Date' },
+                        { type: 'number', label: 'Duration' },
+                        { type: 'number', label: 'Percent Complete' },
+                        { type: 'string', label: 'Dependencies' },
+                    ],
+                    [
+                        'Planning',
+                        'Planning',
+                        'Finished',
+                        new Date(2021, 3, 1),
+                        new Date(2021, 3, 5),
+                        null,
+                        100,
+                        null,
+                    ],
+                    [
+                        'Preparation',
+                        'Preparation',
+                        'In Progress',
+                        new Date(2021, 3, 5),
+                        new Date(2021, 3, 6),
+                        null,
+                        50,
+                        null
+                    ], [
+                        'Procurement',
+                        'Procurement',
+                        'Yet to start',
+                        new Date(2021, 3, 6),
+                        new Date(2021, 3, 10),
+                        null,
+                        0,
+                        null
+                    ],
+
+
+                ]}
+
+                options={{
+                    height: 400,
+                    gantt: {
+                        trackHeight: 30,
+                    },
+                }}
+                rootProps={{ 'data-testid': '1' }}
+            />
+        </div>
+
+    )
+
+}
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,10 +1,28 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
-
+import { useEffect, useRef } from 'react';
+import Gantt from '../components/chart'
 export default function Home() {
+
+
+  const chartRef = useRef(null);
+
+
+
+
+
+
+
+
   return (
+
     <div>
+
       <h1>Chart</h1>
+
+      <Gantt />
     </div>
+
+
   )
 }
